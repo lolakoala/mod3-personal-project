e2cf08b75ac1c0b094332da867e5e75773bca7d6
Lola Brenner <amandadbrenner@gmail.com>
2017-11-08 09:43:34 -0700

    fixes button borders so buttons don't pulse on hover

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import Root from './components/Root';
 import { Provider } from 'react-redux';
 import store from './reducers/configureStore.js';
-// require('./main.scss');
 
 ReactDOM.render(
   <Provider store={store}>
